5ca0deb095bf50ae0c6a48ae065f43e7a0150ccc
IAN BUMATAY <IANBUMATAY.09@GMAIL.COM>
2020-10-12 14:11:09 -0400

    build edit/update controller with view

--- app/controllers/bulletins_controller.rb
+++ app/controllers/bulletins_controller.rb
@@ -4,7 +4,44 @@ class BulletinsController < ApplicationController
       @bulletins = Bulletin.all 
     end 
 
+    def new 
+      @bulletin = Bulletin.new 
+    end 
+
+    def create 
+      @bulletin = Bulletin.new(bulletin_params)
+
+      if @bulletin.save 
+        redirect_to bulletin_path(@bulletin) 
+      else 
+        render :new 
+      end
+    end
+
     def show 
       @bulletin = Bulletin.find(params[:id])
+    end   
+
+    def edit 
+      @bulletin = Bulletin.find(params[:id])
+    end 
+
+    def update 
+      @bulletin = Bulletin.find(params[:id]) 
+      @bulletin.update(bulletin_params) 
+
+      if @bulletin.save 
+        redirect_to bulletins_path
+      else 
+        render :edit 
+      end
     end
+
+    private 
+
+    def bulletin_params 
+        params.require(:bulletin).permit(:title, :content, :rating)
+    end
+
+    
 end
