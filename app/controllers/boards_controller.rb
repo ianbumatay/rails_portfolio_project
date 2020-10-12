45f828c3b159c9fa9a6d982634d99f8c0fac8d8a
IAN BUMATAY <IANBUMATAY.09@GMAIL.COM>
2020-10-12 11:53:16 -0400

    build new/create controller with _form partial for view/new page

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -5,16 +5,28 @@ class BoardsController < ApplicationController
     end  
 
     def new 
-
+      @board = Board.new
     end 
 
     def create 
-
+      @board = Board.new(board_params) 
+      
+      if @board.save 
+        redirect_to board_path(@board) 
+      else 
+        render :new 
+      end
     end
 
     def show 
       @board = Board.find(params[:id])
-    end 
+    end  
+
+    private 
+
+    def board_params 
+        params.require(:users).permit(:month, :title) 
+    end
 
     
 end
