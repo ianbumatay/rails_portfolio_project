00a3cb63ce9ef3d6805a1f277e5df061fde5ee00
IAN BUMATAY <IANBUMATAY.09@GMAIL.COM>
2020-10-14 16:54:58 -0400

    build sessions/new/create/destroy controller and views

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController 
+
+  def new 
+  end 
+
+  def create 
+    #byebug
+    @user = User.find_by(username: params[:user][:username]) 
+    if @user && @user.authenticate(params[:user][:password])
+      session[:user_id] = @user.id 
+      redirect_to user_path(@user)
+    else
+      login_path 
+    end
+  end 
+
+  def destroy 
+    session.delete(:user_id) 
+    redirect_to login_path 
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do 
 
-  resources :users, only: [:create, :show, :edit, :update]
+  resources :users, only: [:create, :show, :edit, :update,]
 
   get '/signup', to: 'users#new' 
 
-  #post '/signup', to: 'users#create'
+  #post '/signup', to: 'users#create' 
+
+  get '/login', to: 'sessions#new' 
+
+  post '/login', to: 'sessions#create' 
+
+  delete '/logout', to: 'sessions#destroy'
 
 
   resources :boards do 
