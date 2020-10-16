740775718a76e3c163413e1f288c44043574c383
IAN BUMATAY <IANBUMATAY.09@GMAIL.COM>
2020-10-16 12:48:55 -0400

    link user to boards model afte signin and login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
     @user = User.find_by(username: params[:user][:username]) 
     if @user && @user.authenticate(params[:user][:password])
       session[:user_id] = @user.id 
-      redirect_to user_path(@user)
+      redirect_to boards_path
     else
       login_path 
     end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
       @user = User.new(user_params) 
       if @user.save
         session[:user_id] = @user.id 
-        redirect_to user_path(@user)
+        redirect_to boards_path
       else 
         render :new 
       end
