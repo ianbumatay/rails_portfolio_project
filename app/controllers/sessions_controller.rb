482ae054fa4afbf5be98117ff576ec971b936d1d
IAN BUMATAY <IANBUMATAY.09@GMAIL.COM>
2020-10-19 22:19:14 -0400

    build third party signup/login using omniauth gem

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -18,6 +18,20 @@ class SessionsController < ApplicationController
   def destroy 
     session.delete(:user_id) 
     redirect_to root_path
+  end 
+
+  def omniauth
+    @user = User.from_omniauth(auth) 
+    @user.save 
+    session[:user_id] = @user.id 
+    redirect_to boards_path 
+  end
+   
+
+  private  
+
+  def auth
+    request.env['omniauth.auth']
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,16 @@ class User < ApplicationRecord
 
     has_secure_password 
 
-    validates :username, :email, presence: true, uniqueness: true
+    validates :username, :email, presence: true, uniqueness: true 
+
+    def self.from_omniauth(auth) 
+      where(email: auth.info.email).first_or_initialize do |user| 
+        user.username = auth.info.name 
+        user.email = auth.info.email 
+        user.password = SecureRandom.hex 
+      end 
+    end
+    
 end
 
 
