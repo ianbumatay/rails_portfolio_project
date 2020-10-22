a735d9bc1604ee375622b409bf337ce1aaf88a0b
IAN BUMATAY <IANBUMATAY.09@GMAIL.COM>
2020-10-22 00:04:47 -0400

    create helper method for bulletin new page

--- app/helpers/bulletins_helper.rb
+++ app/helpers/bulletins_helper.rb
@@ -1,14 +1,25 @@
 module BulletinsHelper
 
-def bulletin_header(board)
-  if board                        
-   "Bulletins for #{board.title}" 
-  else 
-    "All Bulletins in Alpahbetical Order." 
-    end
-  end
-end
+  def bulletin_header(board)
+    if board                        
+     "Bulletins for #{board.title}" 
+    else 
+     "All Bulletins in Alpahbetical Order." 
+     end
+   end
 
-def created_at(bulletin)
-  bulletin.created_at.strftime("Created: %A, %b %e, at %l:%M %p")
+
+  def bulletin_new_header(board)
+    if board 
+      "New Bulletins for #{board.title}" 
+    else 
+       "New Bulletin" 
+     end 
+   end
+
+
+  def created_at(bulletin)
+    bulletin.created_at.strftime("Created: %A, %b %e, at %l:%M %p")
+  end 
+
 end
